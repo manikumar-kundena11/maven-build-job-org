9042a5a827997ee2b37f7a9024a40968c09ad861
manikumar-kundena11 <ragavendramanikumar6@gmail.com>
2026-02-10 11:09:08 +0530

    Update App.java

--- src/main/java/com/exam/App.java
+++ src/main/java/com/exam/App.java
@@ -2,7 +2,7 @@ package com.exam;
 public class App {
     public static void main(String[] args) {
         System.out.println("--------------------------------------------------");
-        System.out.println("JENKINS BUILD SUCCESSFUL! (Output by Manikumar)");
+        System.out.println("JENKINS BUILD SUCCESSFUL! (Output by Manikumar Kundena)");
         System.out.println("--------------------------------------------------");
     }
 }
