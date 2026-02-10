34d9e1a4d1b75593a4c405ae6273906d2bd9558a
manikumar-kundena11 <ragavendramanikumar6@gmail.com>
2026-02-10 11:02:13 +0530

    Add App class with main method for Jenkins output

---
+++ src/main/java/com/exam/App.java
@@ -0,0 +1,8 @@
+package com.exam;
+public class App {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("JENKINS BUILD SUCCESSFUL! (Output by Manikumar)");
+        System.out.println("--------------------------------------------------");
+    }
+}
