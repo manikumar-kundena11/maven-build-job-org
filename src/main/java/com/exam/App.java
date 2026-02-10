09c4e46eb6ae4b064bbf27752704509d91fb5695
manikumar-kundena11 <ragavendramanikumar6@gmail.com>
2026-02-10 16:33:59 +0530

    Update output message in App.java

--- src/main/java/com/exam/App.java
+++ src/main/java/com/exam/App.java
@@ -1,7 +1,7 @@
 package com.exam;
 public class App {
     public static void main(String[] args) {
-        System.out.println("--------------------------------------------------");
+        System.out.println("--------------------------------------------------java -cp target/lab-app-1.0.jar com.exam.App");
         System.out.println("JENKINS BUILD SUCCESSFUL! (Output by Manikumar Kundena)");
         System.out.println("--------------------------------------------------");
     }
